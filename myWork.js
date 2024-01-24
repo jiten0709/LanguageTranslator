63d2b3ada8805e3d185df90ec2526be974e9bacc
jiten0709 <parmar.jiten.microsoft@gmail.com>
2024-01-25 00:30:53 +0530

    latest update in myWork.html

--- myWork.js
+++ myWork.js
@@ -1,9 +1,4 @@
-// import axios from "axios";
-
 const apiUrl = 'https://api.mymemory.translated.net/get';
-
-var recognition = new webkitSpeechRecognition();
-
 
 async function translateText(text, sourceLanguage, targetLanguage) {
     try {
@@ -36,28 +31,17 @@ var startButton = document.getElementById('start');
 var stopButton = document.getElementById('stop');
 var resultElement = document.getElementById('result');
 
+var recognition = new webkitSpeechRecognition();
 
 recognition.lang = window.navigator.language;
 recognition.interimResults = true;
 
-startButton.addEventListener('click', startSpeech);
-stopButton.addEventListener('click', stopSpeech)
-
-function startSpeech() {
-
-    // alert('MIC IS NOW LISTENING')
+startButton.addEventListener('click', () => {
     console.log('start btn clicked')
     recognition.start();
+});
 
-}
-
-// function start() {
-//         console.log('start btn clicked');
-//         recognition.start(); 
-
-// }
-
-function stopSpeech() {
+stopButton.addEventListener('click', () => {
     console.log('stop btn clicked')
     recognition.stop();
     console.log(speechContent);
@@ -70,12 +54,7 @@ function stopSpeech() {
         .catch((error) => {
             console.error('Translation error:', error.message);
         });
-}
-
-// stopButton.addEventListener('click', () => {
-//     // alert('MIC IS NOW OFF')
-
-// });
+});
 
 recognition.addEventListener('result', (event) => {
     const result = event.results[event.results.length - 1][0].transcript;
